897ef0821156c9adee5934474839fdec4f47f9cd
Jiri Kalmar <kalmar.jiri@gmail.com>
2023-06-09 00:21:02 +0200

    Energy difference calculation

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 import numpy as np
 import random
+import matplotlib.pyplot as plt
+#plt.style.use(['science','notebook','grid'])
 
+# ----------------- INITIALIZATION ----------------- #
 J = int(input("Type of interaction:\nferromagnetic: 1\nantiferromagnetic: -1\n--> "))
 N = int(input("Order of the lattice: "))
 
 lattice = np.zeros((N,N))
-energies = np.zeros((N,N))
+
 for i in range(N):
   for j in range(N):
     lattice[i][j] = random.choice((-1,1))
 
 print(lattice)
+
+# -------------------------------------------------- #
   
 
-def calc_energy(lattice, energies):
+def energy_matrix(lattice):
+  en_mat = np.zeros((N,N))
   for i in range(N):
     for j in range(N):
 
@@ -37,11 +43,33 @@ def calc_energy(lattice, energies):
       else:
         S_left = lattice[i][j-1]
 
-      energies[i][j] = -J*lattice[i][j]*(S_top + S_bottom + S_right + S_left)
+      en_mat[i][j] = -J*lattice[i][j]*(S_top + S_bottom + S_right + S_left)
 
-  return energies
+  return en_mat
+
+
+def total_energy(energies):
+  return 0.5*np.sum(energies)
     
-energies= calc_energy(lattice, energies)
+
+def change_rand_spin(lat):
+  i = random.randint(0,N-1)
+  j = random.randint(0,N-1)
+  lat[i][j] *= -1
+
+  return lat
 
 
-print(energies)
+def energy_diff(lattice0, lattice1):
+  en_mat0 = energy_matrix(lattice0)
+  en_mat1 = energy_matrix(lattice1)
+
+  return total_energy(en_mat1) - total_energy(en_mat0)
+
+
+new_lattice = change_rand_spin(lattice.copy())
+dE = energy_diff(lattice, new_lattice)
+
+
+
+#plt.imshow(lattice)
